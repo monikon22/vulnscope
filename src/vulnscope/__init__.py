f1ef1d795eb852141875f3479f00daccb9803da5
Monikon <monikon22@gmail.com>
2026-05-10 15:27:36 +0300

    feat: update CI workflows and versioning strategy

--- src/vulnscope/__init__.py
+++ src/vulnscope/__init__.py
@@ -1,4 +1,10 @@
 """VulnScope package."""
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+
+try:
+    __version__ = version("vulnscope")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
 
